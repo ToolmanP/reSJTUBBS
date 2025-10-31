64076fe7e2cb311d9ec5a3837e899aa78af5882d
Yiyang Wu <toolmanp@tlmp.cc>
2025-10-31 19:56:09 +0800

    feat: add retry_times
    Signed-off-by: Yiyang Wu <toolmanp@tlmp.cc>

--- pkg/worker/common.go
+++ pkg/worker/common.go
@@ -10,18 +10,20 @@ import (
 
 var nthreads = 4
 var wait_interval = 1 * time.Second
+var retry_times = 10
 
 func init() {
 	nthreads = runtime.NumCPU()
 }
 
 func SetNthreads(t int) {
-	nthreads = t 
+	nthreads = t
 }
 
 func VisitWithRetry(c *colly.Collector, url string) {
 	var err error
-	for {
+	for i := range retry_times {
+		var _ = i
 		if err = c.Visit(url); err == nil {
 			break
 		} else {
